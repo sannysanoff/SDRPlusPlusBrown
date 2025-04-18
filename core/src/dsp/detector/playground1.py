f69e6cfef387005f666767510bd4a24b6c28bebf
Sanny Sanoff <san@sysdate.com>
2025-04-18 14:36:38 +0200

    refactor: update file paths in playground1.py for better relative path handling

--- core/src/dsp/detector/playground1.py
+++ core/src/dsp/detector/playground1.py
@@ -1,2 +1,2 @@
-sdrpproot = "/Users/san/Fun/SDRPlusPlus"
-file_path = sdrpproot + 'baseband_14174296Hz_11-08-47_24-02-2024-contest-ssb-small.wav'
+sdrpproot = cwd +  ../../../..
+file_path = sdrpproot + '/tests/test_files/baseband_14174296Hz_11-08-47_24-02-2024-contest-ssb-small.wav'
