9283eeadba4185ec57f3c2600e4bca1d25a193c3
Sanny Sanoff <san@sysdate.com>
2025-04-19 00:26:45 +0200

    style: fix annotation formatting in playground1.jl

--- core/src/dsp/detector/playground1.jl
+++ core/src/dsp/detector/playground1.jl
@@ -157,7 +157,7 @@ function try2()
         # title="Extracted Spectrogram", # Title removed from top
         xformatter = x -> @sprintf("%.0f", x), # Format x-ticks as integers/fixed-point
         xticks = 50, # Suggest more ticks on the x-axis
-+        legend = false, # Disable the legend
+        legend = false, # Disable the legend
         bottom_margin=15Plots.Plots.mm, # Add margin at the bottom for the title
         size=(3600, 700))
     annotate!(plt, [(0.5, -0.1, Plots.text("Extracted Spectrogram", :center, 10))]; annotation_clip=false) # Add title annotation below the plot
